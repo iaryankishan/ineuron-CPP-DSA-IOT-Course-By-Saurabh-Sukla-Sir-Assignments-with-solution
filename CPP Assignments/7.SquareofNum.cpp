10a4b535a09e9d0164030ad3dad96d6f73bd917c
Aryan Kumar <107184052+iaryankishan@users.noreply.github.com>
2022-10-03 00:18:24 +0530

    Create 7.SquareofNum.cpp

---
+++ CPP Assignments/7.SquareofNum.cpp
@@ -0,0 +1,14 @@
+//7. Write a C++ program to calculate the square of a number
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int x,sqr;
+cout << "Enter a number ";
+cin >> x;
+sqr=x*x;
+cout << "Square of " <<x <<" is " <<sqr;
+return 0;
+}
