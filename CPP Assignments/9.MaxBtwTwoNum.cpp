028baa1b6c03c650f4cd5073f089854983e27fd0
Aryan Kumar <107184052+iaryankishan@users.noreply.github.com>
2022-10-03 00:21:48 +0530

    Create 9.MaxBtwTwoNum.cpp

---
+++ CPP Assignments/9.MaxBtwTwoNum.cpp
@@ -0,0 +1,18 @@
+//9. Write a C++ program to find the maximum of two numbers.
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int x,y;
+cout << "Enter a number x=";
+cin >> x;
+cout << "Enter a number y=";
+cin >> y;
+if(x>y)
+cout << x << " is greater";
+else
+cout << y << " is greater";
+return 0;
+}
