2e0a419adcdd051d8ed5bd1e884f3948c1380f94
Aryan Kumar <107184052+iaryankishan@users.noreply.github.com>
2022-10-03 00:17:38 +0530

    Create 6.AverageofThreeNum.cpp

---
+++ CPP Assignments/6.AverageofThreeNum.cpp
@@ -0,0 +1,14 @@
+//6. Write a C++ program to calculate an average of 3 numbers.
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+float Avg,x,y,z;
+cout << "Enter three numbers ";
+cin >> x>>y>>z;
+Avg=(x+y+z)/2;
+cout << "Average of three number is "<<Avg;
+return 0;
+}
